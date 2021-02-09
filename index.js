a09cb38e64a2e839ef8280d2276faee5affce5e5
nicsiv <nicoleusivek@gmail.com>
2021-02-09 12:28:53 -0600

    commit tree 645781412280b6143b3d042b280135cc969f4988

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+const num1 = 60
+const num2 = 2
+const multiply = num1 + num2
+const random = Math.floor(Math.random() * 1000) +1
+const num3 = 16
+const num4 = 4
+const mod = num4 % num3
+const max = Math.max(1,2,4,5,20)
+
+function get62(){
+    console.log(multiply)
+}
+
+function getRandomNumber(){
+    console.log(random)
+}
+
+function getRemainder(){
+    console.log(mod)
+}
+
+function getMax(){
+    console.log(max)
+}
+
+getMax()
+getRemainder()
+get62()
+getRandomNumber()
